f725945a3e3cf3e342de122454bf31e066a9d2a6
kestulka <kgirdiunas0@gmail.com>
2024-03-21 16:41:27 +0200

    asdf

---
+++ calendar.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let calendarEl = document.getElementById("calendar");
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+  });
+  calendar.render();
+});
